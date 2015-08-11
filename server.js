58995038c8024b5e7bd0c02ad8462ebc43672923
Ryan Davis <ryancraigdavis@gmail.com>
2015-08-10 22:46:30 -0700

    Added google analytics, changed site layout adding banner, added modals, added contact us and about us.

--- server.js
+++ server.js
@@ -15,6 +15,47 @@ app.use(bodyParser());
 
 app.use(express.static(__dirname + '/public'));
 
+// var Sequelize = require('sequelize')
+//   , sequelize = new Sequelize('landing_page_jetizen', 'root', null, {
+//       host: 'localhost',
+//       dialect: "mysql", // or 'sqlite', 'postgres', 'mariadb'
+//       port:    3306, // or 5432 (for postgres)
+//     });
+ 
+// sequelize.authenticate().complete(function(err) {
+//     if (!!err) {
+//       console.log('Unable to connect to the database:', err)
+//     } else {
+//       console.log('Connection has been established successfully.')
+//     }
+//   })
+
+//   var medPatient = sequelize.define('medPatient', {
+//   name: Sequelize.STRING,
+//   email: Sequelize.STRING
+// });
+
+//   sequelize.sync().complete(function(err) {
+//      if (!!err) {
+//        console.log('An error occurred while creating the table:', err)
+//      } else {
+//        console.log('It worked!')
+//      }
+//   });
+
+// var patient = medPatient.build({
+//   name: 'Dick Monahaugn',
+//   email: 'test@test.com'
+// })
+ 
+// patient.save().complete(function(err) {
+//     if (!!err) {
+//       console.log('The instance has not been saved:', err)
+//     } else {
+//       console.log(patient)
+//     }
+//   })
+
 app.get('/procedure:name', function(req, res){
   res.setHeader('Content-Type', 'application/json');
     for(var i=0;i<procedureList.getProcedures().length;i++){
